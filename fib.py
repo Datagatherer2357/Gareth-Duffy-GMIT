26d8a77acda412467dbb8a1ddd0cf86bc70c4b28
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-02-05 21:06:28 +0000

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,22 @@
+# Ian McLoughlin
+# A program that displays Fibonacci numbers.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+# Test the function with the following value.
+x = 15
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
+
+
+# My name is Gareth, so the first and last letter of my name (G + H = 7 + 8) give the number  15. The 15th Fibonacci number is 610.
