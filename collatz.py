5fda2bf87708cf0bd23d4a0f2530d64da80f1a2c
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-02-06 21:31:33 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,12 +1,11 @@
 # Gareth Duffy 6-2-2018
-# Program to demonstrate Collatz conjecture sequence
+# A program that demonstrates the Collatz conjecture sequence
 
 x = int(input("Please enter a number to demonstrate the Collatz conjecture: "))
 
 while x > 1:
     if x % 2 == 0:
         x = x / 2
-        round (x)
         print (x)
     else:
         x = 3 * x + 1
