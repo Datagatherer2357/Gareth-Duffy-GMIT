f5ec63e4bc011c60e6b99326c1d9f258c9820399
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-02-07 21:13:46 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -6,9 +6,9 @@ x = int(input("Please enter a number to demonstrate the Collatz conjecture: "))
 while x > 1:
     if x % 2 == 0:
         x = x / 2
-        print (x)
+        print (int(x))
     else:
         x = 3 * x + 1
-        print (x)
+        print (int(x))
     if x == 1:
         break
