88612e30d7dbb67b0d2b4813c7f5931e2dbb5edc
unknown <you@example.com>
2018-02-19 17:56:25 +0000

    Added Loopsum program

--- loopsum.py
+++ loopsum.py
@@ -1,11 +1,12 @@
 # Gareth Duffy 19-2-2018
-# Sum all the numbers from 1 to 100
+# Sum all the even numbers from 1 to 100
 
 sum = 0
 i = 0
 
 while i <= 100:
+  if i % 2 == 0:
     sum = sum + i
-    i = i + 1
+  i = i + 1
 
-print("The sum of all numbers from 1 to 100 is:", sum)
+print("The sum of all even numbers from 1 to 100 is:", sum)
