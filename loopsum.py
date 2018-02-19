8cac33c5c95281850603ace94796c0cbb10063b8
unknown <you@example.com>
2018-02-19 17:33:24 +0000

    loopsum program added

---
+++ loopsum.py
@@ -0,0 +1,11 @@
+# Gareth Duffy 19-2-2018
+# Sum all the numbers from 1 to 100
+
+sum = 0
+i = 0
+
+while i <= 100:
+    sum = sum + i
+    i = i + 1
+
+print("The sum of all numbers from 1 to 100 is:", sum)
