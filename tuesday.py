00651326ddaf0f52f6cddbf509c3210b8660f0fd
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-02-22 22:31:06 +0000

    Update tuesday.py

--- tuesday.py
+++ tuesday.py
@@ -4,6 +4,6 @@
 import datetime
 
 if datetime.datetime.today().weekday() == 1:
-    print("Yay it is Tuesday")
+    print("Yay!! it is Tuesday")
 else:
     print("Unfortunately it is not Tuesday")
