a03c6dafdd4ff1cab06cb10adea60fb5a785876d
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-02-22 19:07:26 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,14 +1,14 @@
 # Gareth Duffy 6-2-2018
 # A program that demonstrates the Collatz conjecture sequence
 
-x = int(input("Please enter a number to demonstrate the Collatz conjecture: "))
+x = int(input("Please enter a number of 17 or more to demonstrate the Collatz conjecture: "))
 
-while x > 1:
-    if x % 2 == 0:
+while x >= 17:  #  Specified starting value of 17 or higher.
+    if x % 2 == 0:  # if loop that divides by 2 if number is even, and returns result.
         x = x / 2
         print (int(x))
-    else:
+    else:  # # else loop that multiplies by 3 if number is odd, and returns result.
         x = 3 * x + 1
         print (int(x))
-    if x == 1:
+    if x == 1:  # if statement that terminates loop once the integer 1 is reached. 
         break
