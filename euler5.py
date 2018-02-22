f288fc0b714f07af817195ad019dfac694a1bb54
unknown <you@example.com>
2018-02-22 18:45:06 +0000

    comment update

--- euler5.py
+++ euler5.py
@@ -3,35 +3,40 @@
 # positive number that is evenly divisible by all of the numbers from 1 to 20
 
 # To find the smallest positive number that is evenly divisible 
-# by all of the numbers from 1 to 20, I realized that I had to find 
+# by all of the numbers from 1 to 20, I discerned that I had to find 
 # the lowest common multiple of these numbers. 
 
-# Importantly, I then came to realise that any numbers which are divisible by 
-# 11 to 20 are similarly divisible by 1 to 10, therefore, for parsimony, 
-# I omitted integers 1 to 10.
+# After some thinking and research, I came to realise that any numbers which 
+# are divisible by 11 to 20 are also divisible by 1 to 10. Therefore I chose to 
+# omit integers 1 to 10 for parsimony.
 
-# I then searched for the formulas of lowest commom multiple (lcm) and also greatest 
-# common divisor (gcd), because gcd is needed as part of the lcm formula. Thus, I
+# I then searched for the formulas for lowest commom multiple (lcm), and for greatest 
+# common divisor (gcd), because gcd was needed as part of the lcm formula. Thus, I
 # would need to assign these formulas as functions in Python.
 
 # gcd formula was extracted from https://codereview.stackexchange.com/questions/95521/gcd-using-euclid-algorithm
 # lcm formula was extracted from https://www.math.nmsu.edu/~pmorandi/CourseMaterials/LCM
 
-# I then created the functions gcd, and lcm. I created gcd first in order to
-# program Python to understand the lcm formula which would follow it. 
+# I then created the functions gcd, and lcm. I scripted gcd first in order to
+# program Python to understand the lcm formula which would follow. 
 
-# result (e5) checked using https://www.calculatorsoup.com/calculators/math/lcm.php
+# The lcm formula is based on finding the least commonn multiple of just two 
+# numbers, so I had to create a for loop incorporating the range function in order
+# to loop over all numbers in the 11 to 20 range.
 
-def gcd(a, b):  # Function of greatest common divisor formula
+# The result (eul5) was chcked using https://www.calculatorsoup.com/calculators/math/lcm.php
+
+
+def gcd(a, b):  # Function for greatest common divisor formula.
   while b:
     a, b = b, a % b
   return a
 
-def lcm(a, b):  # Function of lowest common multiple formula
+def lcm(a, b):  # Function for lowest common multiple formula.
   return (a * b) / gcd(a, b)
 
-e5 = lcm(11, 21)  # e5 (problem solution) variable assigned to adhere to lcm formula
-for i in range(11, 21): # for loop created using "i" variable to iterate over integers ranging 11-20
-  e5 = lcm(i, e5)  # lcm formula loop with i variable to generate smallest positive number solution
+eul5 = lcm(11, 21)  # lcm formula assigned to eul5 (solution) variable.
+for i in range(11, 21): # for loop using "i" variable to iterate over all integers ranging 11-20.
+  eul5 = lcm(i, eul5)  # lcm elements updated to find smallest positive number.
 
-print(e5)
+print(eul5) 
--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,9 @@
 # Gareth Duffy 6-2-2018
-# Fizzbuzz game!!: https://en.wikipedia.org/wiki/Fizz_buzz
-# Synced
+# Fizzbuzz game!: https://en.wikipedia.org/wiki/Fizz_buzz
 
 i = 1
 
-while i <= 100:
+while i <= 100: #Loop through the integers up to 100
     if (i % 3 == 0) and (i % 5 == 0):
         print("FizzBuzz")
     elif i % 3 == 0:
