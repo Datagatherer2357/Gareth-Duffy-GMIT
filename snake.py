f3844665b94f6dc28d405be7d2ae66299810e0d0
unknown <you@example.com>
2018-02-25 11:45:08 +0000

    snake

---
+++ snake.py
@@ -0,0 +1,18 @@
+# Gareth Duffy 25-2-2018
+
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3 Gareth!
+
+                           ____
+                          / o o\\
+                          \  ---<  
+                           \  /
+                 __________/ /
+---'''---'''---=:___________/
+
+:-) Enjoy your programming!!
+"""
+
+
+print(snake_string * how_many_snakes)
