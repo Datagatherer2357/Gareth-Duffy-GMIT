dbb5b4c1f0ec781feba9fcfe82025e5884fef2de
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-04 20:55:26 +0000

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -7,9 +7,9 @@ x = int(input("Please enter a number of 17 or more to demonstrate the Collatz co
 while x >= 17:  #  Specified starting value of 17 or higher.
     if x % 2 == 0:  # if loop that divides by 2 if number is even, and returns result.
         x = x / 2
-        print (int(x))
+        print(int(x))
     else:  # # else loop that multiplies by 3 if number is odd, and returns result.
         x = 3 * x + 1
-        print (int(x))
+        print(int(x))
     if x == 1:  # if statement that terminates loop once the integer 1 is reached. 
         break
