3c15f4e29d9fff06cdb5b78d5f6e6cbd46ad4623
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-05 20:57:02 +0000

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,16 +1,13 @@
- # Gareth Duffy 25-2-2018
-
+# Gareth Duffy 25-2-2018
 # Euler project problem 5: Python program using for and range to calculate the smallest 
-# positive number that is evenly divisible by all of the numbers from 1 to 20.
+# positive number that is evenly divisible by all of the numbers from 1 to 20. (Excercise 4 Programming & Scripting).
 
 # To find the smallest positive number that is evenly divisible 
 # by all of the numbers from 1 to 20, I discerned that I had to find 
 # the lowest common multiple of these numbers. 
-
 # After some thinking and research, I came to realise that any numbers which 
 # are divisible by 11 to 20 are also divisible by 1 to 10. Therefore I chose to 
 # omit integers 1 to 10 for parsimony.
-
 # I then searched for the formulas for lowest commom multiple (lcm), and for greatest 
 # common divisor (gcd), because gcd was needed as part of the lcm formula. Thus, I
 # would need to assign these formulas as functions in Python.
@@ -29,12 +26,12 @@
 
 print("What is the the smallest positive number evenly divisible by all numbers from 1 to 20?")
 
-def gcd(a, b):  # Function for greatest common divisor formula.
+def gcd(a, b):  # Function/algorithm for greatest common divisor.
   while b:
     a, b = b, a % b
   return a
 
-def lcm(a, b):  # Function for lowest common multiple formula.
+def lcm(a, b):  # Function/algorithm for lowest common multiple.
   return (a * b) / gcd(a, b)
 
 eul5 = lcm(11, 20)  # lcm formula assigned to eul5 (solution) variable.
