91dd365615752ee27d49dd5e9dadf58a5cd9a695
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-05 20:46:49 +0000

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# Gareth Duffy 5-3-2018
+# Factorial function algorithm (Excercise 6 Programming & Scripting)
+
+# """Returns the factorial of any integer""" (Docstring)
+
+def factorial(n): # function header 
+  num = n # factorial input number value assigned to num variable
+  for i in range(1, n): # for loop to iterate over all numbers less than factorial input number
+      num = num * i # reassignment of num variable to multliply all looped numbers found with "i"
+  return num # end of factorial function loop
+
+print(factorial(5)) # Calling and printing of factorial function
+print(factorial(7))
+print(factorial(10))
