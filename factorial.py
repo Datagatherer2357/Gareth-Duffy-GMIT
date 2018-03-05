c56cf46d71366a89578d526d8b57abd7bf17bddb
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-05 21:15:22 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -12,3 +12,16 @@ def factorial(n): # function header
 print(factorial(5)) # Calling and printing of factorial function
 print(factorial(7))
 print(factorial(10))
+
+
+# """Version which asks for an integer at input and returns factorial statement"""
+
+n = int(input("Please enter a number to show its factorial: "))
+
+def factorial(n): # function header 
+  num = n # factorial input number value assigned to num variable
+  for i in range(1, n): # for loop to iterate over all numbers less than factorial input number
+      num = num * i # reassignment of num variable to multliply all looped numbers found with "i"
+  return num # end of factorial function loop
+
+print("It's factorial is:", factorial(n), "!")
