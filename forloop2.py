785e8a6558cbe6bbac51f02f2f43bb00941a4ce5
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-19 12:44:56 +0000

    Create forloop2.py

---
+++ forloop2.py
@@ -0,0 +1,6 @@
+# Gareth Duffy 2-3-2018
+# example of for loop using range function as iterator
+# for loops are definite iterators compared to e.g. while loops (indefinite)
+
+for i in range(1, 99, 2): # change range to experiment (third value is the 'step')
+  print(i, end=' ') # end prints all on one line instead of separate lines
