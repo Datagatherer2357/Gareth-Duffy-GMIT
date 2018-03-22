9dab953ae590256319d9c14a1212c75225d7f443
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-03-22 17:04:13 +0000

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -8,16 +8,15 @@
 # After some thinking and research, I came to realise that any numbers which 
 # are divisible by 11 to 20 are also divisible by 1 to 10. Therefore I chose to 
 # omit integers 1 to 10 for parsimony.
-# I then searched for the formulas for lowest commom multiple (lcm), and for greatest 
+# I then obtained formulas for lowest commom multiple (lcm), and for greatest 
 # common divisor (gcd), because gcd was needed as part of the lcm formula. Thus, I
 # would need to assign these formulas as functions in Python.
 
 # gcd formula was extracted from https://codereview.stackexchange.com/questions/95521/gcd-using-euclid-algorithm
 # lcm formula was extracted from https://www.math.nmsu.edu/~pmorandi/CourseMaterials/LCM
 
-# I then created the functions gcd, and lcm. I scripted gcd first in order to
-# program Python to understand the lcm formula which would follow. 
-
+# I created the functions gcd, and lcm. I scripted gcd first in order to
+# program Python to "understand" the lcm formula which would follow. 
 # The lcm formula is based on finding the least commonn multiple of just two 
 # numbers, so I had to create a for loop incorporating the range function in order
 # to loop over all numbers in the 11 to 20 range.
@@ -34,9 +33,10 @@ def gcd(a, b):  # Function/algorithm for greatest common divisor.
 def lcm(a, b):  # Function/algorithm for lowest common multiple.
   return (a * b) / gcd(a, b)
 
-eul5 = lcm(11, 20)  # lcm function assigned to eul5 (solution) variable.
+eul5 = lcm(11, 20)  # lcm function assigned to eul5 (solution) variable. Finds the lcm of 11 and 20.
 for i in range(11, 21): # for loop using "i" variable to iterate over all integers ranging 11-20.
-  eul5 = lcm(eul5, i)  # argument of lcm function updated to yield smallest positive number.
+  eul5 = lcm(eul5, i) # argument of lcm function updated to yield smallest positive number of all integers
+                      # integers ranging 11 to 20. Line of code (altered) was taken from: https://gist.github.com/PEZ   
 
 print("The smallest positive number is", int(eul5)) 
 
