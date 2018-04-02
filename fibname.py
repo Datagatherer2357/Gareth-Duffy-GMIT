41adc19f4933b5e674ae5f53e9393de30c50a5a8
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-04-02 17:49:50 +0100

    Update fibname.py

--- fibname.py
+++ fibname.py
@@ -14,12 +14,13 @@ def fib(n):
   
   return i
 
-name = "Duffy"
-first = name[0]
-last = name[-1]
-firstno = ord(first)
-lastno = ord(last)
-x = firstno + lastno
+name = "Duffy"        # name = "Duffy" # (Assign the string to variable name)
+first = name[0]       # first = name[0] # (First letter of the string name)
+last = name[-1]       # last = name[-1] # (Last letter of the string name)    
+firstno = ord(first)  # firstno = ord(first) # (For my surname ord(D) converts to ASCII code 68)
+lastno = ord(last)    # lastno = ord(last) # (For my surname ord(y) converts to ASCII code 121)
+x = firstno + lastno  # x firstno + lastno # (x (Fibonacci number) is the sum of 68 + 121
+
 
 ans = fib(x)
 print("My surname is", name)
@@ -36,10 +37,5 @@ print("Fibonacci number", x, "is", ans)
 # In very simple terms, ord() returns the ASCII value of a character. For example: ord (‘a’) returns the integer 97, while ord ('A') returns the integer 65. 
 # It gives the opposite result of chr(), e.g. chr (97) is (‘a’).
 
-# To explain:
 
-# name = "Duffy" # (Assign the string to variable name)
-# first = name[0] # (First letter of the string name)# last = name[-1] # (Last letter of the string name)
-# firstno = ord(first) # (For my surname ord(D) converts to ASCII code 68)
-# lastno = ord(last) # (For my surname ord(y) converts to ASCII code 121)
-# x firstno + lastno # (x (Fibonacci number) is the sum of 68 + 121
+
