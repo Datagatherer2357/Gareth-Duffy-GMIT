fa5a378a79a01e2f0667bf9b5657c627db8f1e5f
Gareth D <36170749+Datagatherer2357@users.noreply.github.com>
2018-04-02 17:55:12 +0100

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -4,11 +4,11 @@
 
 x = int(input("Please enter a number to demonstrate the Collatz sequence: ")) # input function request assigned to variable "x"
 
-while x >= 1:  #  Specified starting value of 1 or higher.
-    if x % 2 == 0:  # if loop that divides by 2 if number is even, and returns result.
+while x >= 1:  #  while loop that specifies a starting value of 1 or higher.
+    if x % 2 == 0:  # if statement/condition that divides by 2 if number is even, and returns result.
         x = x / 2 # x is now x divided by 2
         print (int(x))
-    else:  # # else loop that multiplies by 3 if number is odd, and returns result.
+    else:  # # else statement that multiplies by 3 if number is odd, and returns result.
         x = (x * 3) + 1 # x is equal itself multiplied by 3
         print (int(x))
     if x == 1:  # if statement that terminates loop once the integer 1 is reached. 
